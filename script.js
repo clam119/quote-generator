ef2c4237a169ce87b332bfe99415be5250b4049b
Clam119 <Christopher.lam119@gmail.com>
2022-09-05 21:40:35 +0100

    Completed random generated quote

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 //API URL: https://jacintodesign.github.io/quotes-api/data/quotes.json - For Quotes
 let apiQuotes = [];
 
+//Show New Quote
+function newQuote() {
+    //Pick a random quote from apiQuotes array
+    const quotes = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+}
 //Get Quotes From API
 async function getQuotes() {
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
       const response = await fetch(apiUrl); //Response variable populated upon receiving info from api
       apiQuotes = await response.json(); //Getting JSON from API as a response & turnning into a JSON object.
-      console.log(apiQuotes[13]);
+      newQuote();
     } catch (error) {
         //Catch Error Here
     }
